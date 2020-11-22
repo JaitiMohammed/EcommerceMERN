0e9541c1da41f0042af1243129b91c163a360663
jaitimohammed <jaitimohammed.gi@gmail.com>
2020-11-22 17:20:40 +0100

    Modeling our Data

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
+const colors = require("colors");
 const products = require("./data/products");
 
+const connectDB = require("./config/db");
+
 dotenv.config();
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running ...");
@@ -21,5 +25,7 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} on PORT ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on PORT ${PORT}`.yellow.bold
+  )
 );
