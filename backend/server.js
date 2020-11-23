2e4399eb90d31dab37749e6389862361207f71a5
jaitimohammed <jaitimohammed.gi@gmail.com>
2020-11-23 16:34:29 +0100

    Custom Errors Handling

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ const app = express();
 const colors = require("colors");
 // const products = require("./data/products");
 const connectDB = require("./config/db");
-
 const productRoutes = require("./routes/productRoutes");
+
+const errors = require("./middleware/errorMiddleware");
 
 dotenv.config();
 connectDB();
@@ -15,6 +16,9 @@ connectDB();
 // });
 
 app.use("/api/products", productRoutes);
+
+app.use(errors.notFound);
+app.use(errors.errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(
